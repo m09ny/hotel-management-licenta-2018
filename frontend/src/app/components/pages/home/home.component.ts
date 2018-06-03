86dc33b9ff65d3fb3a5d6c4eb20597145cce6315
Monica Valcu <m09ny@yahoo.com>
2018-06-04 00:54:23 +0300

    few updates on the frontend html files

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../service/';
 import { MenuItem } from 'primeng/components/common/menuitem';
+import {ButtonModule} from 'primeng/components/button/button';
+
+import { Accomodations, Rooms, Clients, Bills, Reservations } from '../../../models/';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  
-  constructor() { }
+  rangeDates: Date[];
+  bills: Bills[];
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    let today = new Date();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let prevMonth = (month === 0) ? 11 : month -1;
+    let prevYear = (prevMonth === 11) ? year - 1 : year;
+    let nextMonth = (month === 11) ? 0 : month + 1;
+    let nextYear = (nextMonth === 0) ? year + 1 : year;
+    
+    let invalidDate = new Date();
+    invalidDate.setDate(today.getDate() - 1);
+
+    this.apiService.get('bills/').subscribe(res => {
+      this.bills = res;
+    });
+  }
+
+  viewBill(select: Bills) {
+    console.log(JSON.stringify(select));
+
   }
 
 }
